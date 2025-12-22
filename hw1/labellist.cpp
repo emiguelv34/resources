e141c4f437c51e245840ec2f268b5a490fd3ff36
Mark Redekopp <redekopp@usc.edu>
2025-12-22 13:15:42 -0800

    Initial import from 2022

---
+++ hw1/labellist.cpp
@@ -0,0 +1,252 @@
+#include "labellist.h"
+#include <cassert>
+
+// Complete - Do not alter
+LabelList::MsgNode::MsgNode(const MsgType& msg)
+    : msg_(msg)
+{
+    // Every message is part of the "all" (index 0) label
+    labelled_.push_back(true);
+    next_.push_back(nullptr);
+    prev_.push_back(nullptr);
+}
+
+
+// Complete - Do not alter
+LabelList::MsgToken::MsgToken()
+ : node_(nullptr), list_(nullptr)
+{
+}
+
+// To be completed - must adhere to any requirements in the .h file
+LabelList::MsgToken::MsgToken(MsgNode* node, LabelList* list)
+// Add any initializers here
+
+{
+
+}
+
+// To be completed - must adhere to any requirements in the .h file
+bool LabelList::MsgToken::operator==(const MsgToken& rhs) const
+{
+
+}
+
+// Complete - Do not alter
+bool LabelList::MsgToken::operator!=(const MsgToken& rhs) const
+{
+    return !operator==(rhs);
+}
+
+// To be completed - must adhere to any requirements in the .h file
+LabelList::MsgToken::operator bool() const
+{
+
+}
+
+// Complete - Do not alter
+bool LabelList::MsgToken::isValid() const
+{
+    return this->node_ != nullptr;
+}
+
+// Complete - Do not alter
+LabelList::MsgToken& LabelList::MsgToken::next(const LabelType& label)
+{
+    size_t index = this->list_->findLabelIndex(label);
+    if( false == this->isLabelMember(index) ) 
+    {
+        throw std::invalid_argument("Node is not part of the given label");
+    }
+    this->node_ = this->node_->next_[index];
+
+    // Should return reference to self
+    return *this;
+}
+
+// Complete - Do not alter
+LabelList::MsgToken& LabelList::MsgToken::prev(const LabelType& label)
+{
+    size_t index = this->list_->findLabelIndex(label);
+    if( false == this->isLabelMember(index) ) 
+    {
+        throw std::invalid_argument("Node is not part of the given label");
+    }
+    this->node_ = this->node_->prev_[index];
+
+    // Should return reference to self
+    return *this;
+}
+
+// Complete - Do not alter
+bool LabelList::MsgToken::isLabelMember(const LabelType& label) const
+{
+    size_t index = this->list_->findLabelIndex(label);
+    return isLabelMember(index);
+}
+
+// Complete - Do not alter
+bool LabelList::MsgToken::isLabelMember(size_t index) const
+{
+    return this->isValid() && 
+            index != INVALID_LABEL &&
+            index < this->node_->labelled_.size() &&
+            this->node_->labelled_[index] ;
+}
+
+// To be completed - must adhere to any requirements in the .h file
+LabelList::MsgType const & LabelList::MsgToken::msg() const
+{
+
+}
+// To be completed - must adhere to any requirements in the .h file
+LabelList::MsgType& LabelList::MsgToken::msg() 
+{
+
+}
+
+// To be completed - must adhere to any requirements in the .h file
+std::ostream& operator<<(std::ostream& ostr, const LabelList::MsgToken& token)
+{
+    if(token.isValid())
+    {
+        ostr << token.node_->msg_;
+    }
+    return ostr;
+}
+
+// Static constant 
+const LabelList::MsgToken LabelList::end_(nullptr, nullptr);
+
+// To be completed - must adhere to any requirements in the .h file
+LabelList::LabelList() 
+// Add any initializers here
+
+{
+
+}
+
+// To be completed - must adhere to any requirements in the .h file
+LabelList::~LabelList()
+{
+
+}
+
+// To be completed - must adhere to any requirements in the .h file
+void LabelList::clear()
+{
+
+}
+
+// To be completed - must adhere to any requirements in the .h file
+bool LabelList::empty() const
+{
+
+}
+
+// Complete
+size_t LabelList::size() const
+{
+    MsgNode* n = this->heads_[0];
+    size_t cnt = 0;
+    while(n != nullptr){
+        cnt++;
+        n = n->next_[0];
+    }
+    return cnt;
+}
+
+// To be completed - must adhere to any requirements in the .h file
+LabelList::MsgToken LabelList::add(const MsgType& msg)
+{
+
+}
+
+
+// To be completed - must adhere to any requirements in the .h file
+void LabelList::remove(const MsgToken& token)
+{
+
+}
+
+// To be completed - must adhere to any requirements in the .h file
+void LabelList::label(const MsgToken& token, const LabelType& label)
+{
+    if(!token.isValid())
+    {
+        throw std::invalid_argument("Bad token in label()");
+    }
+
+}
+
+// To be completed - must adhere to any requirements in the .h file
+void LabelList::unlabel(const MsgToken& token, const LabelType& label)
+{
+    if(!token.isValid())
+    {
+        throw std::invalid_argument("Bad token in label()");
+    }
+
+
+}
+
+// Complete - Do not alter
+LabelList::MsgToken LabelList::find(const MsgType& msg, const LabelType& label)
+{
+    // See if the label exists, will throw if not
+    size_t level = this->getLabelIndex(label);
+    MsgNode* n = this->heads_[level];
+    while(n != nullptr)
+    {
+        if(n->msg_ == msg)
+        {
+            return MsgToken(n, this);
+        }
+        n = n->next_[level];
+    }
+    return end();
+}
+
+// To be completed - must adhere to any requirements in the .h file
+LabelList::MsgToken LabelList::find(size_t index, const LabelType& label)
+{
+
+}
+
+// Complete - Do not alter
+LabelList::MsgToken const & LabelList::end() const
+{
+    return end_;
+}
+
+// Complete - Do not alter
+void LabelList::print(std::ostream& ostr, const LabelType& label, char separator) const
+{
+    size_t level = this->getLabelIndex(label);
+    MsgNode* n = this->heads_[level];
+    while(nullptr != n)
+    {
+        ostr << n->msg_ << separator;
+        n = n->next_[level];
+    }
+    ostr << std::endl;
+}
+
+// To be completed - must adhere to any requirements in the .h file
+size_t LabelList::findLabelIndex(const LabelType& label) const
+{
+
+}
+
+// Complete - Do not alter
+size_t LabelList::getLabelIndex(const LabelType& label) const
+{
+    size_t retval = this->findLabelIndex(label);
+    if(INVALID_LABEL == retval)
+    {
+        throw std::out_of_range("Label doesn't exist");
+    }
+    return retval;
+}
+
+
